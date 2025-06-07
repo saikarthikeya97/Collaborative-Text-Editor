3233a97d31efef3e671c3ea59f76b2e5608c2002
saikarthikeya97 <karthikeya7098@gmail.com>
2025-06-07 16:41:47 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,86 @@
-// src/App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import TextEditor from "./TextEditor";
-import './index.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+import './styles.css';
+
+const SAVE_INTERVAL_MS = 2000;
+const TOOLBAR_OPTIONS = [
+  ['bold', 'italic', 'underline', 'strike'],
+  ['blockquote', 'code-block'],
+  [{ header: 1 }, { header: 2 }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ indent: '-1' }, { indent: '+1' }],
+  [{ direction: 'rtl' }],
+  [{ size: ['small', false, 'large', 'huge'] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ color: [] }, { background: [] }],
+  [{ font: [] }],
+  [{ align: [] }],
+  ['clean'],
+];
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Redirect root path to a default document */}
-        <Route path="/" element={<Navigate to="/documents/123" replace />} />
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
-        {/* Dynamic route to load the editor for document by id */}
-        <Route path="/documents/:id" element={<TextEditor />} />
-      </Routes>
-    </Router>
-  );
+  useEffect(() => {
+    const s = io('/');
+    setSocket(s);
+    return () => s.disconnect();
+  }, []);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on('receive-changes', handler);
+
+    return () => {
+      socket.off('receive-changes', handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    const interval = setInterval(() => {
+      socket.emit('save-document', quill.getContents());
+    }, SAVE_INTERVAL_MS);
+
+    return () => clearInterval(interval);
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (!socket || !quill) return;
+
+    socket.once('load-document', (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    socket.emit('get-document', 'default-document');
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (!wrapper) return;
+
+    wrapper.innerHTML = '';
+    const editor = document.createElement('div');
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText('Loading document...');
+    setQuill(q);
+  }, []);
+
+  return <div className="container" ref={wrapperRef}></div>;
 }
 
 export default App;
